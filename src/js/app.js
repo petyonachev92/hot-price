085c37801e94f45e0d51141640c1534ac06a6877
Petyo <pyo@abv.bg>
2021-12-27 22:13:15 +0200

    feat: add fire emoji to hot product

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,11 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  addFireToHot();
 });
+
+function addFireToHot() {
+  let hot = document.getElementsByClassName('hot')[0];
+  let price = hot.textContent;
+  hot.textContent = price + '🔥';
+}
